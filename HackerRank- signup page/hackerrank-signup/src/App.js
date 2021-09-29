707aab7b4375604301eb7fda0a6602db87ff2a08
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2021-09-29 11:46:27 +0330

    add hackerrank logo

--- HackerRank- signup page/hackerrank-signup/src/App.js
+++ HackerRank- signup page/hackerrank-signup/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import IconInput from './iconInput/iconInput';
+import './app.css';
 
 function App() {
   return (
-    <div>
-      <IconInput icon="./user.svg" hint="First & Last name" ></IconInput>
-      <IconInput icon="./email.svg" hint="Email" ></IconInput>
-      <IconInput icon="./lock.svg" hint="Your password" ></IconInput>
+    <div className="auth-container">
+      <div className="auth-content-wrap">
+        <div className="auth-headline">
+          <div className="logo-wraper">
+            <img  src="./hr.svg"></img>
+          </div>
+          <h2>For Developers</h2>
+          <p>Practice coding, prepare for interviews, and get hired.</p>
+        </div>
+        <div>
+          <IconInput icon="./user.svg" hint="First & Last name" ></IconInput>
+          <IconInput icon="./email.svg" hint="Email" ></IconInput>
+          <IconInput icon="./lock.svg" hint="Your password" ></IconInput>
+        </div>
+      </div>
     </div>
   );
 }
--- HackerRank- signup page/hackerrank-signup/src/iconInput/iconInput.js
+++ HackerRank- signup page/hackerrank-signup/src/iconInput/iconInput.js
@@ -4,7 +4,7 @@ import './iconInput.css'
 function IconInput(props) {
 
     return (
-        <div>
+        <div className="icon-input">
             <img src={ props.icon } alt="icon" />
             <span><input placeholder={ props.hint }></input></span>
         </div>
