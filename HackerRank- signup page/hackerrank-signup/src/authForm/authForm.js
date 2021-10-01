f01b2bec6a78d4e2c39bdc201b6eb575a87f25f3
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2021-10-01 12:57:55 +0330

    add tab buttons and shadows

--- HackerRank- signup page/hackerrank-signup/src/authForm/authForm.js
+++ HackerRank- signup page/hackerrank-signup/src/authForm/authForm.js
@@ -1,12 +1,23 @@
-import React, { Children } from 'react';
+import React, { useState } from 'react';
 import "./authForm.css"
 
 function AuthForm(props) {
+  const [isSignupActiveTab, setSignupActiveTab] = useState(true);
+
+  function changeTab(e, isActive) {
+    e.preventDefault();
+    setSignupActiveTab(isActive);
+  }
+
     return (
       <div className="auth-form">
         <div className="tab-header">
-          <a className="tab-button" href="#">Sign up</a>
-          <a className="tab-button" href="#">Log in</a>
+          <a className={`tab-button ${isSignupActiveTab ? 'tab-button-active' : ''}`}
+             onClick={ (e) => changeTab(e, true) } href="#"
+             style={ isSignupActiveTab ? { boxShadow: "5px 0px 5px -5px rgb(0 0 0 / 20%)", zIndex: 1 } : {} } >Sign up</a>
+          <a className={`tab-button ${!isSignupActiveTab ? 'tab-button-active' : ''}`}
+             onClick={ (e) => changeTab(e, false) } href="#"
+             style={ !isSignupActiveTab ? { boxShadow: "-5px 0px 5px -5px rgb(0 0 0 / 20%)", zIndex: 1 } : {} } >Log in</a>
         </div>
         <div className="tab-list-content">
           { props.children }
