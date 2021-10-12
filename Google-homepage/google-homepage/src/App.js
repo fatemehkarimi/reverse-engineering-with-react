cb72af1fac8419afc1fee77e6c7a091cfda733e1
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2021-10-12 12:21:47 +0330

    add top bar menu items

--- Google-homepage/google-homepage/src/App.js
+++ Google-homepage/google-homepage/src/App.js
@@ -1,6 +1,22 @@
+import React from 'react';
+import "./App.css"
+
 function App() {
   return (
-    <div></div>
+    <div className="flex-horizontal">
+      <div className="flex-row top-menu">
+        <a className="link-primary" href="#">Gmail</a>
+        <a className="link-primary" href="#">Images</a>
+        <svg focusable="false" viewBox="0 0 24 24">
+          <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z">
+          </path>
+        </svg>
+        <button className="btn btn-primary">Sign in</button>
+      </div>
+      <div>Image</div>
+      <div>SearchBox</div>
+      <div>About</div>
+    </div>
   );
 }
 
