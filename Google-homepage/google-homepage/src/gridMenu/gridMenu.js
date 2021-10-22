7a12b41f4e18a2995878b3c12f3f863e45e76c18
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2021-10-22 19:02:50 +0330

    add icons to menu

--- Google-homepage/google-homepage/src/gridMenu/gridMenu.js
+++ Google-homepage/google-homepage/src/gridMenu/gridMenu.js
@@ -1,17 +1,35 @@
 import React, { useState } from 'react';
 import MenuItem from './menuItem/menuItem';
 import { ReactComponent as MenuIcon } from "../icons/menu.svg";
-import './gridMenu.css'
+import MeetLogo from '../icons/meet.png';
+import GmailLogo from '../icons/gmail.png';
+import NewsLogo from '../icons/news.png';
+import PlayLogo from '../icons/play.png';
+import YoutubeLogo from '../icons/youtube.png';
+import MapsLogo from '../icons/maps.png';
+import SearchLogo from '../icons/search.png';
+import ChatLogo from '../icons/chat.png';
+import ContactsLogo from '../icons/contacts.png';
+import DriveLogo from '../icons/drive.png';
+import CalendarLogo from '../icons/calendar.png';
+import TranslateLogo from '../icons/translate.png';
+import './gridMenu.css';
+
 
 function Menu() {
     return (<div className="menu-container">
-        <MenuItem src="../icons/search.png" name="Search" />
-        <MenuItem src="../icons/maps.png" name="Maps" />
-        <MenuItem src="../icons/youtube.png" name="YouTube" />
-        <MenuItem src="../icons/play.png" name="Play" />
-        <MenuItem src="../icons/news.png" name="News" />
-        <MenuItem src="../icons/gmail.png" name="Gmail" />
-        <MenuItem src="../icons/meet.png" name="Meet" />
+        <MenuItem src={ SearchLogo } name="Search" />
+        <MenuItem src={ MapsLogo } name="Maps" />
+        <MenuItem src={ YoutubeLogo } name="YouTube" />
+        <MenuItem src={ PlayLogo } name="Play" />
+        <MenuItem src={ NewsLogo } name="News" />
+        <MenuItem src={ GmailLogo } name="Gmail" />
+        <MenuItem src={ MeetLogo } name="Meet" />
+        <MenuItem src={ ChatLogo } name="Chat" />
+        <MenuItem src={ ContactsLogo } name="Contacts" />
+        <MenuItem src={ DriveLogo } name="Drive" />
+        <MenuItem src={ CalendarLogo } name="Calendar" />
+        <MenuItem src={ TranslateLogo } name="Translate" />
     </div>);
 }
 
