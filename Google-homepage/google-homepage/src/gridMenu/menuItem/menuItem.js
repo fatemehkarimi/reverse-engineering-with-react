58272c8cdc5f7d35d6124020dbdbcaa5a21bf989
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2021-10-24 12:51:00 +0330

    center image logos in tools menu

--- Google-homepage/google-homepage/src/gridMenu/menuItem/menuItem.js
+++ Google-homepage/google-homepage/src/gridMenu/menuItem/menuItem.js
@@ -4,6 +4,7 @@ import "./menuItem.css";
 function MenuItem(props) {
     return (<div className="grid-menu-item">
         <img src={ props.src } alt={ props.name } />
+        <span>{ props.name }</span>
     </div>)
 }
 
