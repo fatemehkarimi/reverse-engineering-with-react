34cc7e91f2dcbc2e08557c75f3bc2e5eb644150f
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2021-10-26 17:35:00 +0330

    extending search box when focused

--- Google-homepage/google-homepage/src/searchbox/searchBox.js
+++ Google-homepage/google-homepage/src/searchbox/searchBox.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as SearchIcon } from '../icons/search.svg';
 import { ReactComponent as MicrophoneIcon } from '../icons/microphone.svg';
 import "./searchBox.css";
 
+function SearchHistory() {
+    return (<div className="search-history">
+        <ul>
+            <li>salam</li>
+            <li>Iran university of science and technology</li>
+            <li>Isfahan university of technology</li>
+            <li>golestan</li>
+        </ul>
+    </div>);   
+}
+
 function SearchBox(props) {
+    const [displaySearchHistory, setDisplaySearchHistory] = useState(false);
+
+    function handleInputClick() {
+        setDisplaySearchHistory(true);
+    }
+
     return(
-    <div className="search-box">
+    <div className={ `${displaySearchHistory ? "search-box-expanded" : "search-box"}` }>
+      <div className="search-input-container">
         <SearchIcon className="search-box-icon" />
-        <input name="q"/>
+        <input name="q" className="search-box-input" onClick={ handleInputClick }
+            onBlur={ () => setDisplaySearchHistory(false) } />
         <MicrophoneIcon className="search-box-icon" />
+      </div>
+        { displaySearchHistory && <SearchHistory /> }
     </div>);
 }
 
